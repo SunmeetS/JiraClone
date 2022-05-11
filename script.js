17bb41b37119c0c2c99f0caa2a22a23323b08d91
Sunmeet Singh <renu.kabeer@gmail.com>
2022-05-12 00:55:57 +0530

    Jira Almost Done

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ let taskEditor = document.querySelector(".taskEditor")
 let addTaskEditor = true;
 let textArea = document.querySelector(".textArea");
 let footer = document.querySelector('.footer');
+let deleteBtn = document.querySelector(".dustbin")
+let deletee = false
+let ticketCont1;
+
+deleteBtn.addEventListener('click',function(){
+    deletee =! deletee
+})
 
 addButton.addEventListener("click",function(){
     if(addTaskEditor){
@@ -12,7 +19,10 @@ addButton.addEventListener("click",function(){
         taskEditor.style.display = "none"
     }
     addTaskEditor =!addTaskEditor
+    
 })
+
+
 
 taskEditor.addEventListener("keydown",function(e){
     let key = e.key;
@@ -24,18 +34,37 @@ taskEditor.addEventListener("keydown",function(e){
     }
 })
 
+
+
 function createTicket(value){
-    // <div class="ticketCont">
-    //         <div class="ticketColour"></div>
-    //         <div class="ticketId"> #abc12 </div>
-    //         <div class="taskArea">some task</div>
-    //     </div>
+ 
     let ticketCont = document.createElement("div")
     ticketCont.setAttribute('class','ticketCont')
     ticketCont.innerHTML = `<div class="ticketColour"></div>
                             <div class = "ticketDiv">
-                                <div class="ticketId"> #abc12 </div>
+                                <div class="ticketId"> #abc12 
+                                    <div class="dustbin">
+                                    <i class="fa fa-trash"></i>
+                                    </div>
+                                </div>
                             </div>
                             <div class="taskArea">${value}</div>`
     footer.appendChild(ticketCont)
+    ticketCont1 = document.querySelectorAll(".ticketCont");
+    
+    for(let i = 0; i<ticketCont1.length; i++){
+        deleteBtn.addEventListener('click',function(){
+            if(deletee == true){
+                ticketCont1[i].style.boxShadow = '10px 10px 1rem red, -10px 10px 1rem red'
+            }
+            else{
+                ticketCont1[i].style.boxShadow = 'none'
+            }
+        })
+        
+    }
+
+}
+if(deletee){
+    
 }
